7e955cc083408d73a83d9ecbbf10da31604696e2
RyuKyoungHyun <soccergh@naver.com>
2023-06-14 18:03:48 +0900

    230614 중간 업로드

--- solo/solo_web/solo_web.js
+++ solo/solo_web/solo_web.js
@@ -1,5 +1,15 @@
-function fileName () {
-  const name = document.getElementsById('photo1').value;
+function fileName1 () {
+  const photo1 = document.getElementById('photo1').files[0].name;
 
-  document.getElementById('photo1').innerText = name;
+  document.getElementById('fileName1').innerText = photo1;
+}
+function fileName2 () {
+  const photo2 = document.getElementById('photo2').files[0].name;
+
+  document.getElementById('fileName2').innerText = photo2;
+}
+function fileName3 () {
+  const photo3 = document.getElementById('photo3').files[0].name;
+
+  document.getElementById('fileName3').innerText = photo3;
 }
