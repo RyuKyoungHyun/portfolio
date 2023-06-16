33492a3dc8c315cf01b83b7725940cb7dfcaa5e4
RyuKyoungHyun <soccergh@naver.com>
2023-06-16 16:03:59 +0900

    230616 4시 중간 업로드, 자기소개박스 높이 수정 필요

--- solo/solo_web/solo_web.js
+++ solo/solo_web/solo_web.js
@@ -1,15 +1,15 @@
 function fileName1 () {
   const photo1 = document.getElementById('photo1').files[0].name;
 
-  document.getElementById('fileName1').innerText = photo1;
+  document.getElementById('fileName1').textContent = photo1;
 }
 function fileName2 () {
   const photo2 = document.getElementById('photo2').files[0].name;
 
-  document.getElementById('fileName2').innerText = photo2;
+  document.getElementById('fileName2').textContent = photo2;
 }
 function fileName3 () {
   const photo3 = document.getElementById('photo3').files[0].name;
 
-  document.getElementById('fileName3').innerText = photo3;
+  document.getElementById('fileName3').textContent = photo3;
 }
