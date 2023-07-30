78f5e3886f080c2df0dbd2e4d08402106499242a
RyuKyoungHyun <soccergh@naver.com>
2023-07-31 08:33:05 +0900

    주말정산

--- solo/cloneCoding/minhPham.js
+++ solo/cloneCoding/minhPham.js
@@ -1,13 +1,13 @@
-addEventListener('scroll', () => {
-  console.log(scrollY);
-  const firstVideoPoint = document.querySelector('.firstScreen video');
+addEventListener("scroll", () => {
+  // console.log(scrollY);
+  const firstVideoPoint = document.querySelector(".firstScreen video");
   const vH = document.documentElement.clientHeight;
   // firstScreen 비디오 움직이기
   if (scrollY >= 0) {
     firstVideoPoint.style.transform = `translateY(${-scrollY / 2}px)`;
   }
   // aboutline Event
-  const aboutLines = document.querySelectorAll('.aboutMe .textLine');
+  const aboutLines = document.querySelectorAll(".aboutMe .textLine");
   let parentTop = aboutLines[0].offsetParent.offsetTop;
   let lineTop = aboutLines[0].offsetTop;
   let lineHt = aboutLines[0].clientHeight;
@@ -22,7 +22,7 @@ addEventListener('scroll', () => {
   });
 
   // what I do Event
-  const idoLines = document.querySelectorAll('.whatIDo .textLine');
+  const idoLines = document.querySelectorAll(".whatIDo .textLine");
   parentTop = idoLines[0].offsetParent.offsetTop;
   lineTop = idoLines[0].offsetTop;
   lineHt = idoLines[0].clientHeight;
@@ -33,7 +33,7 @@ addEventListener('scroll', () => {
   });
 
   // experience Event
-  const experLines = document.querySelectorAll('.experience .textLine');
+  const experLines = document.querySelectorAll(".experience .textLine");
   parentTop = experLines[0].offsetParent.offsetTop;
   lineTop = experLines[0].offsetTop;
   lineHt = experLines[0].clientHeight;
@@ -47,7 +47,7 @@ addEventListener('scroll', () => {
     }
   });
   // 배경 따라가기
-  const experience = document.querySelector('.experience');
+  const experience = document.querySelector(".experience");
   const experHt = experience.clientHeight;
   if (scrollY >= experience.offsetTop - vH) {
     experience.style.backgroundPositionY = `${
@@ -56,7 +56,7 @@ addEventListener('scroll', () => {
   }
 
   // clients Event
-  const clientLines = document.querySelectorAll('.clients .textLine');
+  const clientLines = document.querySelectorAll(".clients .textLine");
   parentTop = clientLines[0].offsetParent.offsetTop;
   lineTop = clientLines[0].offsetTop;
   lineHt = clientLines[0].clientHeight;
@@ -71,7 +71,7 @@ addEventListener('scroll', () => {
   });
 
   //planet Event
-  const planetLines = document.querySelectorAll('.planetList .textLine');
+  const planetLines = document.querySelectorAll(".planetList .textLine");
   parentTop = planetLines[0].offsetParent.offsetTop;
   lineTop = planetLines[0].offsetTop;
   lineHt = planetLines[0].clientHeight;
@@ -82,8 +82,8 @@ addEventListener('scroll', () => {
   });
 
   // rasingVideo Event
-  const raisingTop = document.querySelector('.raisingVideo').offsetTop;
-  const videowrap = document.querySelector('.raisingVideo .video-wrapper');
+  const raisingTop = document.querySelector(".raisingVideo").offsetTop;
+  const videowrap = document.querySelector(".raisingVideo .video-wrapper");
   const videoMaxHt = Math.trunc(((scrollY - (raisingTop - vH)) / vH) * 100);
   if (scrollY > raisingTop - vH) {
     if (videoMaxHt <= 100) {
@@ -91,10 +91,10 @@ addEventListener('scroll', () => {
     }
   }
   // what they1 Event
-  const theyList = document.querySelectorAll('.whatThey .content-box > li');
+  const theyList = document.querySelectorAll(".whatThey .content-box > li");
   theyList.forEach((list) => {
     // const theyLines = document.querySelectorAll('.whatThey .textLine');
-    const theyLines = list.querySelectorAll('.whatThey .textLine');
+    const theyLines = list.querySelectorAll(".whatThey .textLine");
     parentTop = theyLines[0].offsetParent.offsetParent.offsetTop;
     lineTop = theyLines[0].offsetTop;
     lineHt = theyLines[0].clientHeight;
@@ -109,21 +109,21 @@ addEventListener('scroll', () => {
     });
   });
   // 인물 움직이기
-  const theyTop = document.querySelector('.whatThey').offsetTop;
+  const theyTop = document.querySelector(".whatThey").offsetTop;
   const theyListTop = document.querySelector(
-    '.whatThey .content-box > li'
+    ".whatThey .content-box > li"
   ).clientHeight;
-  const theyHt = document.querySelector('.whatThey').clientHeight;
-  const personList = document.querySelector('.whatThey .personList');
+  const theyHt = document.querySelector(".whatThey").clientHeight;
+  const personList = document.querySelector(".whatThey .personList");
   const personListTop = document.querySelector(
-    '.whatThey .personList'
+    ".whatThey .personList"
   ).offsetTop;
-  const personLi = document.querySelector('.whatThey .personList li');
-  const personLis = document.querySelectorAll('.whatThey .personList li');
+  const personLi = document.querySelector(".whatThey .personList li");
+  const personLis = document.querySelectorAll(".whatThey .personList li");
   const theyListHt = document.querySelector(
-    '.whatThey .content-box > li'
+    ".whatThey .content-box > li"
   ).clientHeight;
-  const personSVG = document.querySelector('.whatThey .personList svg');
+  const personSVG = document.querySelector(".whatThey .personList svg");
   const personGap = (personList.clientHeight - personLi.clientHeight * 3) / 2;
   // console.log(personGap);
   // console.log(personSVG);
@@ -134,30 +134,30 @@ addEventListener('scroll', () => {
     // console.log((theyHt / 3) * 1);
     // 인물리스트 이동 효과
     if (scrollY - theyTop > (theyListHt / 2) * 3) {
-      personLis[0].lastElementChild.classList.remove('look');
-      personLis[1].lastElementChild.classList.remove('look');
-      personLis[2].lastElementChild.classList.add('look');
+      personLis[0].lastElementChild.classList.remove("look");
+      personLis[1].lastElementChild.classList.remove("look");
+      personLis[2].lastElementChild.classList.add("look");
 
       personSVG.style.transform = `translateY(${
         (personLi.clientHeight + personGap) * 2
       }px) rotate(-135deg)`;
     } else if (scrollY - theyTop > theyListHt / 2) {
-      personLis[0].lastElementChild.classList.remove('look');
-      personLis[1].lastElementChild.classList.add('look');
-      personLis[2].lastElementChild.classList.remove('look');
+      personLis[0].lastElementChild.classList.remove("look");
+      personLis[1].lastElementChild.classList.add("look");
+      personLis[2].lastElementChild.classList.remove("look");
       personSVG.style.transform = `translateY(${
         personLi.clientHeight + personGap
       }px) rotate(-135deg)`;
     } else {
-      personLis[0].lastElementChild.classList.add('look');
-      personLis[1].lastElementChild.classList.remove('look');
-      personLis[2].lastElementChild.classList.remove('look');
+      personLis[0].lastElementChild.classList.add("look");
+      personLis[1].lastElementChild.classList.remove("look");
+      personLis[2].lastElementChild.classList.remove("look");
       personSVG.style.transform = `translateY(0px) rotate(-135deg)`;
     }
   }
 
   // motto 배경 따라가기
-  const motto = document.querySelector('.myMotto');
+  const motto = document.querySelector(".myMotto");
   const mottoHt = motto.clientHeight;
   if (scrollY >= motto.offsetTop - vH) {
     motto.style.backgroundPositionY = `${(scrollY - motto.offsetTop) / 2}px`;
@@ -176,16 +176,32 @@ addEventListener('scroll', () => {
   //     fill.style.width = `0%`;
   //   }
   // });
+
+  const menuOrange = document.querySelectorAll(".container.orange .menu a");
+  const menuDark = document.querySelectorAll(".container.dark .menu a");
+  if (scrollY > motto.offsetTop) {
+    menuDark[2].classList.add("active");
+    menuDark[1].classList.remove("active");
+    menuDark[0].classList.remove("active");
+  } else if (scrollY > experience.offsetTop) {
+    menuDark[2].classList.remove("active");
+    menuDark[1].classList.add("active");
+    menuDark[0].classList.remove("active");
+  } else {
+    menuDark[2].classList.remove("active");
+    menuDark[1].classList.remove("active");
+    menuDark[0].classList.add("active");
+  }
 });
 
 //mask cursor
-const maskContainer = document.querySelector('.container.orange');
+const maskContainer = document.querySelector(".container.orange");
 
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 let cursorX = 0;
 let cursorY = 0;
 let cursorSize = 2.4;
-document.addEventListener('mousemove', (e) => {
+document.addEventListener("mousemove", (e) => {
   cursorX = e.clientX;
   cursorY = e.clientY;
   maskContainer.style.webkitMaskPosition = `calc(${cursorX}px - ${
@@ -194,29 +210,29 @@ document.addEventListener('mousemove', (e) => {
   // console.log(maskContainer.style.webkitMaskSize);
 });
 function bigger(a) {
-  a.addEventListener('mouseover', () => {
+  a.addEventListener("mouseover", () => {
     maskContainer.style.webkitMaskSize = `25vw`;
     cursorSize = 25;
   });
-  a.addEventListener('mouseout', () => {
+  a.addEventListener("mouseout", () => {
     maskContainer.style.webkitMaskSize = `2.4vw`;
     cursorSize = 2.4;
   });
 }
-const firstH2 = document.querySelector('.container.orange .firstScreen h2');
+const firstH2 = document.querySelector(".container.orange .firstScreen h2");
 const aboutContent = document.querySelector(
-  '.container.orange .aboutMe .content-box'
+  ".container.orange .aboutMe .content-box"
 );
 const experContent = document.querySelector(
-  '.container.orange .experience .content-box'
+  ".container.orange .experience .content-box"
 );
 const clientContent = document.querySelector(
-  '.container.orange .clients .text-box'
+  ".container.orange .clients .text-box"
 );
 const theycontent = document.querySelectorAll(
-  '.container.orange .whatThey .text-wrapper'
+  ".container.orange .whatThey .text-wrapper"
 );
-const mottocontent = document.querySelector('.container.orange .myMotto h2');
+const mottocontent = document.querySelector(".container.orange .myMotto h2");
 bigger(firstH2);
 bigger(aboutContent);
 bigger(experContent);
@@ -228,13 +244,13 @@ bigger(mottocontent);
 
 //hover 영역조정
 const whatHoverLook = document.querySelectorAll(
-  '.container.dark .whatIDo .hover-look'
+  ".container.dark .whatIDo .hover-look"
 );
 const whatText = document.querySelectorAll(
-  '.container.dark .whatIDo .textLine'
+  ".container.dark .whatIDo .textLine"
 );
 const whatorangeText = document.querySelectorAll(
-  '.container.orange .whatIDo .textLine'
+  ".container.orange .whatIDo .textLine"
 );
 
 //whatIDo 호버효과
@@ -243,14 +259,14 @@ whatText.forEach((a) => {
   a.nextElementSibling.style.top = `${a.offsetTop + aHt / 2}px`;
 });
 whatorangeText.forEach((w, idx) => {
-  w.addEventListener('mouseover', () => {
+  w.addEventListener("mouseover", () => {
     whatHoverLook[idx].style.top = `${whatText[idx].offsetTop}px`;
     whatHoverLook[idx].style.height = `${whatText[idx].clientHeight}px`;
 
     maskContainer.style.webkitMaskSize = `0vw`;
     cursorSize = 0;
   });
-  w.addEventListener('mouseout', () => {
+  w.addEventListener("mouseout", () => {
     whatHoverLook[idx].style.top = `${
       whatText[idx].offsetTop + whatText[idx].clientHeight / 2
     }px`;
@@ -263,13 +279,13 @@ whatorangeText.forEach((w, idx) => {
 
 //planet
 const planetHoverLook = document.querySelectorAll(
-  '.container.dark .planetList .hover-look'
+  ".container.dark .planetList .hover-look"
 );
 const planetText = document.querySelectorAll(
-  '.container.dark .planetList .textLine'
+  ".container.dark .planetList .textLine"
 );
 const planetOrangeText = document.querySelectorAll(
-  '.container.orange .planetList .textLine'
+  ".container.orange .planetList .textLine"
 );
 
 planetText.forEach((a) => {
@@ -277,14 +293,14 @@ planetText.forEach((a) => {
   a.nextElementSibling.style.top = `${a.offsetTop + aHt / 2}px`;
 });
 planetOrangeText.forEach((w, idx) => {
-  w.addEventListener('mouseover', () => {
+  w.addEventListener("mouseover", () => {
     planetHoverLook[idx].style.top = `${planetText[idx].offsetTop}px`;
     planetHoverLook[idx].style.height = `${planetText[idx].clientHeight}px`;
 
     maskContainer.style.webkitMaskSize = `0vw`;
     cursorSize = 0;
   });
-  w.addEventListener('mouseout', () => {
+  w.addEventListener("mouseout", () => {
     planetHoverLook[idx].style.top = `${
       planetText[idx].offsetTop + planetText[idx].clientHeight / 2
     }px`;
@@ -297,16 +313,16 @@ planetOrangeText.forEach((w, idx) => {
 
 //history
 const historyHoverLook = document.querySelectorAll(
-  '.container.dark .history .hover-look'
+  ".container.dark .history .hover-look"
 );
 const historyText = document.querySelectorAll(
-  '.container.dark .history .textLine'
+  ".container.dark .history .textLine"
 );
 const historyOrangeText = document.querySelectorAll(
-  '.container.orange .history-text'
+  ".container.orange .history-text"
 );
 const historyH3 = document.querySelectorAll(
-  '.container.dark .history .hover-look h3'
+  ".container.dark .history .hover-look h3"
 );
 
 historyText.forEach((a) => {
@@ -317,14 +333,14 @@ historyText.forEach((a) => {
   });
 });
 historyOrangeText.forEach((w, idx) => {
-  w.addEventListener('mouseover', () => {
+  w.addEventListener("mouseover", () => {
     historyHoverLook[idx].style.top = `${historyText[idx].offsetTop}px`;
     historyHoverLook[idx].style.height = `${historyText[idx].clientHeight}px`;
 
     maskContainer.style.webkitMaskSize = `0vw`;
     cursorSize = 0;
   });
-  w.addEventListener('mouseout', () => {
+  w.addEventListener("mouseout", () => {
     historyHoverLook[idx].style.top = `${
       historyText[idx].offsetTop + historyText[idx].clientHeight / 2
     }px`;
@@ -336,12 +352,11 @@ historyOrangeText.forEach((w, idx) => {
 });
 
 // fixed icon Event
-const logo = document.querySelectorAll('.container .logo');
+const logo = document.querySelectorAll(".container .logo");
 const vW = document.documentElement.clientWidth;
-console.log(vW);
-document.addEventListener('mouseover', (e) => {
-  logo[1].addEventListener('mousemove', (lo) => {
-    // console.log(logo[0]);
+// console.log(vW);
+document.addEventListener("mouseover", (e) => {
+  logo[1].addEventListener("mousemove", (lo) => {
     logo[0].style.transform = `translate(${e.clientX - (vW * 6.083) / 100}px, ${
       e.clientY - (vW * 6.083) / 100
     }px)`;
@@ -351,9 +366,141 @@ document.addEventListener('mouseover', (e) => {
     }px)`;
   });
 });
-logo[1].addEventListener('mouseout', () => {
+logo[1].addEventListener("mouseout", () => {
   // console.log(logo[0]);
   logo[0].style.transform = `translate(0px, 0px)`;
   logo[0].style.fill = `rgb(183, 171, 152)`;
   logo[1].style.transform = `translate(0px, 0px)`;
+});
+
+// footer hover 효과
+
+const footerOrangeLinks = document.querySelectorAll(
+  ".container.orange footer .content-box a"
+);
+const footerDarkLinks = document.querySelectorAll(
+  ".container.dark footer .content-box a .block"
+);
+footerOrangeLinks.forEach((a, idx) => {
+  a.addEventListener("mouseover", () => {
+    footerDarkLinks[idx].style.top = `0px`;
+    footerDarkLinks[idx].style.height = `${a.clientHeight}px`;
+
+    maskContainer.style.webkitMaskSize = `0vw`;
+    cursorSize = 0;
+  });
+  a.addEventListener("mouseout", () => {
+    footerDarkLinks[idx].style.top = `50%`;
+    footerDarkLinks[idx].style.height = `0px`;
+
+    maskContainer.style.webkitMaskSize = `2.4vw`;
+    cursorSize = 2.4;
+  });
+});
+
+// footer sms hover 효과
+const footerOrangeSms = document.querySelectorAll(
+  ".container.orange footer .email-tel .sms"
+);
+const footerDarkSms = document.querySelectorAll(
+  ".container.dark footer .email-tel .sms .hover-look"
+);
+footerOrangeSms.forEach((f, idx) => {
+  f.addEventListener("mouseover", () => {
+    footerDarkSms[idx].style.top = `0px`;
+    footerDarkSms[idx].style.height = `${f.clientHeight}px`;
+
+    maskContainer.style.webkitMaskSize = `0vw`;
+    cursorSize = 0;
+  });
+  f.addEventListener("mouseout", () => {
+    footerDarkSms[idx].style.top = `50%`;
+    footerDarkSms[idx].style.height = `0px`;
+
+    maskContainer.style.webkitMaskSize = `2.4vw`;
+    cursorSize = 2.4;
+  });
+});
+const menuOrange = document.querySelectorAll(".container.orange .menu a");
+const menuDark = document.querySelectorAll(".container.dark .menu a");
+const motto = document.querySelector(".myMotto");
+const experience = document.querySelector(".experience");
+menuOrange.forEach((m, idx) => {
+  m.addEventListener("mouseover", () => {
+    menuDark[idx].classList.add("active");
+
+    maskContainer.style.webkitMaskSize = `0vw`;
+    cursorSize = 0;
+  });
+  m.addEventListener("mouseout", () => {
+    if (scrollY > motto.offsetTop) {
+      menuDark[0].classList.remove("active");
+      menuDark[1].classList.remove("active");
+    } else if (scrollY > experience.offsetTop) {
+      menuDark[0].classList.remove("active");
+      menuDark[2].classList.remove("active");
+    } else {
+      menuDark[1].classList.remove("active");
+      menuDark[2].classList.remove("active");
+    }
+
+    maskContainer.style.webkitMaskSize = `2.4vw`;
+    cursorSize = 2.4;
+  });
+});
+// 스크롤 이동
+const experTop = document.querySelector(
+  ".container.orange .experience"
+).offsetTop;
+const mottoTop = document.querySelector(".container.orange .myMotto").offsetTop;
+menuOrange.forEach((m) => {
+  m.addEventListener("click", (a) => {
+    a.preventDefault();
+  });
+});
+menuOrange[0].addEventListener("click", () => {
+  scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+menuOrange[1].addEventListener("click", () => {
+  scrollTo({
+    top: experTop,
+    behavior: "smooth",
+  });
+});
+menuOrange[2].addEventListener("click", () => {
+  scrollTo({
+    top: mottoTop,
+    behavior: "smooth",
+  });
+});
+
+const fixedOrangeLists = document.querySelectorAll(
+  ".container.orange .fixed-footer a"
+);
+const fixedDarkLists = document.querySelectorAll(
+  ".container.dark .fixed-footer a"
+);
+console.log(fixedDarkLists);
+const fixedListTop = document.querySelector(
+  ".container .fixed-footer ul"
+).offsetTop;
+
+// console.log(fixedListTop);
+fixedOrangeLists.forEach((li, idx) => {
+  console.log(li.offsetTop);
+  li.addEventListener("mousemove", (e) => {
+    e.target.style.transform = `translate(${cursorX - (vW * 5.483) / 100}px,${
+      cursorY - (fixedListTop + e.target.offsetTop)
+    }px)`;
+    fixedDarkLists[idx].style.transform = `translate(${
+      cursorX - (vW * 5.483) / 100
+    }px,${cursorY - (fixedListTop + e.target.offsetTop)}px)`;
+  });
+  li.addEventListener("mouseout", (e) => {
+    e.target.style.transform = `translate(0px,0px)`;
+    fixedDarkLists[idx].style.transform = `translate(0px,0px)`;
+  });
 });
